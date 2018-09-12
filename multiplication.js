ee57bd14bca21d1d462af8aff05d016251b402d7
Kazuya Takami <foundationmeister@outlook.com>
2018-09-12 17:22:24 +0900

    commit.

--- multiplication.js
+++ multiplication.js
@@ -7,11 +7,15 @@ $(function() {
     result,
     $taget = $("#multiplication");
     
-  for(i = 1; i < 10; i++) {
+  for(i = 0; i < 10; i++) {
     $taget.append($("<tr>").attr("id", "line" + i));
     for(j = 1; j < 10; j++) {
-      result = i * j;
-      $("#line" + i).append($("<td>").text(i + "*" + j + "=" + result));
+      if (i === 0) {
+        $("#line" + i).append($("<th>").text(j + "の段"));
+      } else {
+        result = j * i;
+        $("#line" + i).append($("<td>").text(j + "*" + i + "=" + result));
+      }
     }
   }
 });
