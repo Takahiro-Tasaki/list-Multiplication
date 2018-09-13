f9d819d8d5d6673672863ed2fe5dc0e584b204e4
Takahiro-Tasaki <qxgxyea_h@yahoo.co.jp>
2018-09-13 15:28:26 +0900

    順次処理のみ

--- multiplication.js
+++ multiplication.js
@@ -1,21 +1,113 @@
 $(function() {
   "use strict";
   
-  var
-    i,
-    j,
-    result,
-    $taget = $("#multiplication");
-    
-  for(i = 0; i < 10; i++) {
-    $taget.append($("<tr>").attr("id", "line" + i));
-    for(j = 1; j < 10; j++) {
-      if (i === 0) {
-        $("#line" + i).append($("<th>").text(j + "の段"));
-      } else {
-        result = j * i;
-        $("#line" + i).append($("<td>").text(j + "*" + i + "=" + result));
-      }
-    }
-  }
+  $("#multiplication").append($("<tr>").attr("id", "line0"));
+  $("#line0").append($("<th>").text("1の段"));
+  $("#line0").append($("<th>").text("2の段"));
+  $("#line0").append($("<th>").text("3の段"));
+  $("#line0").append($("<th>").text("4の段"));
+  $("#line0").append($("<th>").text("5の段"));
+  $("#line0").append($("<th>").text("6の段"));
+  $("#line0").append($("<th>").text("7の段"));
+  $("#line0").append($("<th>").text("8の段"));
+  $("#line0").append($("<th>").text("9の段"));
+  
+  $("#multiplication").append($("<tr>").attr("id", "line1"));
+  $("#line1").append($("<td>").text("1×1=1"));
+  $("#line1").append($("<td>").text("2×1=2"));
+  $("#line1").append($("<td>").text("3×1=3"));
+  $("#line1").append($("<td>").text("4×1=4"));
+  $("#line1").append($("<td>").text("5×1=5"));
+  $("#line1").append($("<td>").text("6×1=6"));
+  $("#line1").append($("<td>").text("7×1=7"));
+  $("#line1").append($("<td>").text("8×1=8"));
+  $("#line1").append($("<td>").text("9×1=9"));
+  
+  $("#multiplication").append($("<tr>").attr("id", "line2"));
+  $("#line2").append($("<td>").text("1×2=2"));
+  $("#line2").append($("<td>").text("2×2=4"));
+  $("#line2").append($("<td>").text("3×2=6"));
+  $("#line2").append($("<td>").text("4×2=8"));
+  $("#line2").append($("<td>").text("5×2=10"));
+  $("#line2").append($("<td>").text("6×2=12"));
+  $("#line2").append($("<td>").text("7×2=14"));
+  $("#line2").append($("<td>").text("8×2=16"));
+  $("#line2").append($("<td>").text("9×2=18"));
+  
+  $("#multiplication").append($("<tr>").attr("id", "line3"));
+  $("#line3").append($("<td>").text("1×3=3"));
+  $("#line3").append($("<td>").text("2×3=6"));
+  $("#line3").append($("<td>").text("3×3=9"));
+  $("#line3").append($("<td>").text("4×3=12"));
+  $("#line3").append($("<td>").text("5×3=15"));
+  $("#line3").append($("<td>").text("6×3=18"));
+  $("#line3").append($("<td>").text("7×3=21"));
+  $("#line3").append($("<td>").text("8×3=24"));
+  $("#line3").append($("<td>").text("9×3=27"));
+  
+  $("#multiplication").append($("<tr>").attr("id", "line4"));
+  $("#line4").append($("<td>").text("1×4=4"));
+  $("#line4").append($("<td>").text("2×4=8"));
+  $("#line4").append($("<td>").text("3×4=12"));
+  $("#line4").append($("<td>").text("4×4=16"));
+  $("#line4").append($("<td>").text("5×4=20"));
+  $("#line4").append($("<td>").text("6×4=24"));
+  $("#line4").append($("<td>").text("7×4=28"));
+  $("#line4").append($("<td>").text("8×4=32"));
+  $("#line4").append($("<td>").text("9×4=36"));
+  
+  $("#multiplication").append($("<tr>").attr("id", "line5"));
+  $("#line5").append($("<td>").text("1×5=5"));
+  $("#line5").append($("<td>").text("2×5=10"));
+  $("#line5").append($("<td>").text("3×5=15"));
+  $("#line5").append($("<td>").text("4×5=20"));
+  $("#line5").append($("<td>").text("5×5=25"));
+  $("#line5").append($("<td>").text("6×5=30"));
+  $("#line5").append($("<td>").text("7×5=35"));
+  $("#line5").append($("<td>").text("8×5=40"));
+  $("#line5").append($("<td>").text("9×5=45"));
+  
+  $("#multiplication").append($("<tr>").attr("id", "line6"));
+  $("#line6").append($("<td>").text("1×6=6"));
+  $("#line6").append($("<td>").text("2×6=12"));
+  $("#line6").append($("<td>").text("3×6=18"));
+  $("#line6").append($("<td>").text("4×6=24"));
+  $("#line6").append($("<td>").text("5×6=30"));
+  $("#line6").append($("<td>").text("6×6=36"));
+  $("#line6").append($("<td>").text("7×6=42"));
+  $("#line6").append($("<td>").text("8×6=48"));
+  $("#line6").append($("<td>").text("9×6=54"));
+  
+  $("#multiplication").append($("<tr>").attr("id", "line7"));
+  $("#line7").append($("<td>").text("1×7=7"));
+  $("#line7").append($("<td>").text("2×7=14"));
+  $("#line7").append($("<td>").text("3×7=21"));
+  $("#line7").append($("<td>").text("4×7=28"));
+  $("#line7").append($("<td>").text("5×7=35"));
+  $("#line7").append($("<td>").text("6×7=42"));
+  $("#line7").append($("<td>").text("7×7=49"));
+  $("#line7").append($("<td>").text("8×7=56"));
+  $("#line7").append($("<td>").text("9×7=63"));
+  
+  $("#multiplication").append($("<tr>").attr("id", "line8"));
+  $("#line8").append($("<td>").text("1×8=8"));
+  $("#line8").append($("<td>").text("2×8=16"));
+  $("#line8").append($("<td>").text("3×8=24"));
+  $("#line8").append($("<td>").text("4×8=32"));
+  $("#line8").append($("<td>").text("5×8=40"));
+  $("#line8").append($("<td>").text("6×8=48"));
+  $("#line8").append($("<td>").text("7×8=56"));
+  $("#line8").append($("<td>").text("8×8=64"));
+  $("#line8").append($("<td>").text("9×8=72"));
+  
+  $("#multiplication").append($("<tr>").attr("id", "line9"));
+  $("#line9").append($("<td>").text("1×9=9"));
+  $("#line9").append($("<td>").text("2×9=18"));
+  $("#line9").append($("<td>").text("3×9=27"));
+  $("#line9").append($("<td>").text("4×9=36"));
+  $("#line9").append($("<td>").text("5×9=45"));
+  $("#line9").append($("<td>").text("6×9=54"));
+  $("#line9").append($("<td>").text("7×9=63"));
+  $("#line9").append($("<td>").text("8×9=72"));
+  $("#line9").append($("<td>").text("9×9=81"));
 });
